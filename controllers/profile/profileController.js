2eed6497442f2bd745a757f2e1847448af239a84
Kolya <58349866+iluvkoohi@users.noreply.github.com>
2022-07-31 23:05:00 +0800

    #073122 update profileController.js

--- controllers/profile/profileController.js
+++ controllers/profile/profileController.js
@@ -4,31 +4,11 @@ const deleteProfile = async (req, res) => {
   try {
     const accountId = req.params.id;
 
-    Customer.findOneAndRemove({ accountId })
-      .then((value) => {
-        if (value != null) {
-          return res.status(200).json({ message: "deleted" });
-        }
-      })
-      .catch(() => {});
+    Customer.findOneAndRemove({ accountId });
+    Merchant.findOneAndRemove({ accountId });
+    Rider.findOneAndRemove({ accountId });
 
-    Merchant.findOneAndRemove({ accountId })
-      .then((value) => {
-        if (value != null) {
-          return res.status(200).json({ message: "deleted" });
-        }
-      })
-      .catch(() => {});
-
-    Rider.findOneAndRemove({ accountId })
-      .then((value) => {
-        if (value != null) {
-          return res.status(200).json({ message: "deleted" });
-        }
-      })
-      .catch(() => {});
-
-    return res.status(400).json({ message: "failed" });
+    return res.status(200).json({ message: "success" });
   } catch (error) {
     console.log(error);
   }
